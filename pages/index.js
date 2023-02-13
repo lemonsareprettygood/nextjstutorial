9c4cd7d6ada06153739b862e522d0e70526e05f7
lemonsareprettygood <wizzy.erik@gmail.com>
2023-02-13 21:37:06 +0200

    feat: first commit for today

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Link from 'next/link';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+export default function Home() {
+  return (
+      <Layout home>
+        <Head>
+          <title>{siteTitle}</title>
+        </Head>
+        <section className={utilStyles.headingMd}>
+          <p>[Hello my name is Cock, BigCocK]</p>
+          <p>
+            (This is a sample website - you’ll be building a site like this on{' '}
+            <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          </p>
+        </section>
+      </Layout>
+  );
+}
