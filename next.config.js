b24a22e25d44d4583131191f665001b7256d9907
lemonsareprettygood <wizzy.erik@gmail.com>
2023-02-15 20:51:30 +0200

    feat: correct asset domain for github pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 /**
  * @type {import('next').NextConfig}
  */
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+    // trim off `<owner>/`
+    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+    assetPrefix = `/${repo}/`
+    basePath = `/${repo}`
+}
+
 const nextConfig = {
+    assetPrefix: assetPrefix,
+    basePath: basePath,
     images: {
         unoptimized: true
     }
